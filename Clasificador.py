6eabb81e6a9ceb06640517bd4f67d1ed6421b4a3
byLugos <bylugos147@gmail.com>
2024-03-03 22:42:22 -0500

    Proceso de  modularidad

--- Clasificador.py
+++ Clasificador.py
@@ -1,33 +1,61 @@
 import re
 import os
+import shutil
 import Lector as l
 
-class Ingles:
+class Metodo:
     def __init__(self, ruta_carpeta):
         self.ruta_carpeta = ruta_carpeta
+    def clasificarIngles(self, level):
+        buscarIngles = re.compile(r'INGLES?:?\s*', re.IGNORECASE)
+        buscarInglesAlt = re.compile(r'INGLÉS?:?\s*', re.IGNORECASE)
 
-    def clasificar(self, level):
-        buscar_ingles = re.compile(r'INGLES?:?\s*', re.IGNORECASE)
-        buscar_ingles_dos = re.compile(r'INGLÉS?:?\s*', re.IGNORECASE)
-        carpeta_b1 = 'NIVEL B1 INGLES'
-        carpeta_b2 = 'NIVEL B2 INGLES'
+        #CARPETA A ENVIAR AL SERVIDOR
+        carpetaB1 = 'NIVEL B1 INGLES'
+        carpetaB2 = 'NIVEL B2 INGLES'
 
         if not os.path.isdir(self.ruta_carpeta):
             raise ValueError("La carpeta no existe.")
 
-        for nombre_archivo in os.listdir(self.ruta_carpeta):
-            if nombre_archivo.endswith('.pdf'):
-                ruta_completa = os.path.join(self.ruta_carpeta, nombre_archivo)
-                texto = l.leerPDF(ruta_completa)
+        for nombreArchivo in os.listdir(self.ruta_carpeta):
+            if nombreArchivo.endswith('.pdf'):
+                rutaCompleta = os.path.join(self.ruta_carpeta, nombreArchivo)
+                texto = l.leerPDF(rutaCompleta)
 
-                if buscar_ingles.search(texto) or buscar_ingles_dos.search(texto):
+                if buscarIngles.search(texto) or buscarInglesAlt.search(texto):
                     if level == "B1" and "B1" in texto:
-                        print(f"Copiando el archivo {nombre_archivo} a la carpeta de B1...")
-                        # shutil.copy(ruta_completa, carpeta_b1)
+                        print(f"Copiando el archivo {nombreArchivo} a la carpeta de B1...")
+                        #shutil.copy(rutaCompleta, carpetaB1)
                         print("¡Archivo copiado con éxito!\n")
                     elif level == "B2" and "B2" in texto:
-                        print(f"Copiando el archivo {nombre_archivo} a la carpeta de B2...")
-                        # shutil.copy(ruta_completa, carpeta_b2)
+                        print(f"Copiando el archivo {nombreArchivo} a la carpeta de B2...")
+                        #shutil.copy(rutaCompleta, carpetaB2)
                         print("¡Archivo copiado con éxito!\n")
                 else:
-                    print("NO HAY NIVEL DE INGLÉS REGISTRADO EN EL PDF", nombre_archivo)
+                    print("NO HAY NIVEL DE INGLÉS REGISTRADO EN EL PDF", nombreArchivo)
+
+    def clasificarExp(self):
+        buscarExp = re.compile(r'experiencia|historial|trayectoria', re.IGNORECASE)
+        totalAnho = 0
+        totalSemana = 0
+
+        if not os.path.isdir(self.ruta_carpeta):
+            raise ValueError("La carpeta no existe.")
+
+        for nombreArchivo in os.listdir(self.ruta_carpeta):
+            if nombreArchivo.endswith('.pdf'):
+                rutaCompleta = os.path.join(self.ruta_carpeta, nombreArchivo)
+                texto = l.leerPDF(rutaCompleta)
+
+                if buscarExp.search(texto):
+                    # Buscar números que representen años y semanas de experiencia
+                    anos = re.findall(r'(\d+)\s+años?', texto)
+                    semanas = re.findall(r'(\d+)\s+semanas?', texto)
+
+                    # Sumar los años y semanas encontrados
+                    if anos:
+                        totalAnho += sum(map(int, anos))
+                    if semanas:
+                        totalSemana += sum(map(int, semanas))
+
+        print(f"Total de experiencia: {totalAnho} años y {totalSemana} semanas.")
--- main.py
+++ main.py
@@ -1,34 +1,66 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
 import Clasificador as clasif
 class GUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Clasificador de CV")
-        self.clasificadorIngles = clasif.Ingles("Persistencia")
-        self.create_widgets()
+        self.clasificador = clasif.Metodo("Persistencia")
+        self.crearVentanas()
 
-    def create_widgets(self):
-        self.label = tk.Label(self.root, text="Carpeta predeterminada: Persistencia")
-        self.label.pack()
+    def crearVentanas(self):
+        self.main_frame = ttk.Frame(self.root, padding="20")
+        self.main_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        self.main_frame.columnconfigure(0, weight=1)
 
-        self.b1_button = tk.Button(self.root, text="Clasificar por nivel de inglés (B1)", command=lambda: self.clasificar_ingles("B1"))
-        self.b1_button.pack()
+        self.label = ttk.Label(self.main_frame, text="Carpeta predeterminada: Persistencia")
+        self.label.grid(column=0, row=0, columnspan=2, pady=(0, 10))
 
-        self.b2_button = tk.Button(self.root, text="Clasificar por nivel de inglés (B2)", command=lambda: self.clasificar_ingles("B2"))
-        self.b2_button.pack()
+        self.b1_button = ttk.Button(self.main_frame, text="Clasificar por nivel de inglés (B1)", command=lambda: self.ClasifIngles("B1"))
+        self.b1_button.grid(column=0, row=1, pady=5, sticky=tk.W+tk.E)
 
-        self.other_button = tk.Button(self.root, text="Clasificar por otras categorías", command=self.etc)
-        self.other_button.pack()
+        self.b2_button = ttk.Button(self.main_frame, text="Clasificar por nivel de inglés (B2)", command=lambda: self.ClasifIngles("B2"))
+        self.b2_button.grid(column=1, row=1, pady=5, sticky=tk.W+tk.E)
 
-    def etc(self):
-        # Aquí puedes agregar tu lógica para clasificar por otras categorías
-        pass
+        self.b3_button = ttk.Button(self.main_frame, text="Clasificar por tiempo de experiencia", command=lambda: self.ClasifExp())
+        self.b3_button.grid(column=2, row=1, pady=5, sticky=tk.W+tk.E)
 
-    def clasificar_ingles(self, level):
+        for child in self.main_frame.winfo_children():
+            child.grid_configure(padx=10)
+
+        self.centrarVentana()
+
+    def centrarVentana(self):
+        # Obtener el ancho y alto de la pantalla
+        ancho = self.root.winfo_screenwidth()
+        alto = self.root.winfo_screenheight()
+
+        # Obtener el ancho y alto de la ventana
+        anchoPantalla = self.root.winfo_reqwidth()
+        altoPantalla = self.root.winfo_reqheight()
+
+        # Calcular las coordenadas x y y para centrar la ventana
+        x = int((ancho / 2) - (anchoPantalla / 2))
+        y = int((alto / 2) - (altoPantalla / 2))
+
+        # Establecer la geometría de la ventana
+        self.root.geometry(f"+{x}+{y}")
+
+
+    def ClasifIngles(self, level):
         try:
-            self.clasificadorIngles.clasificar(level)
+            #Llamar a la clase clasificador, específicamente la de inglés
+            self.clasificador.clasificarIngles(level)
+            #Si sale bien, sale clasificación completa
             messagebox.showinfo("Clasificación completada", f"Los archivos han sido clasificados en la carpeta 'NIVEL {level} INGLES'.")
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
+    def ClasifExp(self):
+        try:
+            #Llamar a la clase clasificador, específicamente la de inglés
+            self.clasificador.clasificarExp()
+            #Si sale bien, sale clasificación completa
+            messagebox.showinfo("Clasificación completada", f"Los archivos han sido clasificados en la carpeta 'AÑOS'")
         except ValueError as e:
             messagebox.showerror("Error", str(e))
 def main():
